8ee3d97df1b98e189c18d9f6115cb21f2b68c57f
Adeyemi <30883466+atoyegbe@users.noreply.github.com>
2018-05-02 00:37:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,24 +22,20 @@ let fact= document.querySelector("#fact");
 let factText = document.querySelector("#factText");
 
 let numberInput = document.querySelector("#numberInput");
-numberInput.addEventListener('input', getFactAjax);
-
-function getFactAjax() {
+numberInput.addEventListener('input', getFactFetch);
+  
+function getFactFetch() {
   let number = numberInput.value;
-
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', ' http://numbersapi.com/'+number);
-
-  xhr.onload = function() {
-    if(this.status == 200 && number != '' ) {
-       fact.style.display == 'show';
-       factText.innerText = this.responseText;
+ fetch('http://numbersapi.com/'+number)
+  .then(response => response.text())
+  .then(data => {
+    if(number != '') {
+      fact.style.display == 'block';
+      factText.innerText = data;
     }
-  }
-
-  xhr.send();
+  })
+  .catch(err => console.log(err))
 }
-
 
 
 
